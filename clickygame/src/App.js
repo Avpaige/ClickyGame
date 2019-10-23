21e1f6241fc8f39a437d8017048f854ebc2324cf
Ari Virgil-Paige <avpaige@gmail.com>
2019-10-22 22:35:20 -0700

    initial files and rough header

---
+++ clickygame/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/index';
+
+function App() {
+  return (
+   <Header></Header>
+  );
+}
+
+export default App;
---
+++ clickygame/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(props) {
+    return (
+        <nav>
+            Ocean's Guess
+            Click an image to begin and earn points! But click more than once and this heist is blown!
+            Score: {`${props.score}`}   |     High Score:  {`${props.highScore}`}
+        </nav>
+    )
+}
+
+export default Header;
